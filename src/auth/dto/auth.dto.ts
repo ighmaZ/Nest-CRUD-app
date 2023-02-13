7499b8e110a7ce00b9bb1027903750374193bd77
ighmaz <ighmaz@ighmazs-MacBook-Air.local>
2023-02-13 19:49:57 +0530

    fix

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,10 @@
-export interface AuthDto {
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
